598c7350a0fcd140a8b0a2b75189803043c9c6a4
Omkar Kulkarni <omkarak45@gmail.com>
2020-12-01 14:52:08 +0530

    Added switch tab logic

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
-window.addEventListener('load',function(){
-    document.querySelector('body').classList.add("loaded")  
+window.addEventListener("load", function () {
+  document.querySelector("body").classList.add("loaded");
+});
+
+const toggleButton = document.querySelector(".toggle-menu");
+const navBar = document.querySelector(".nav-bar");
+const navLink = document.querySelectorAll(".nav-link");
+toggleButton.addEventListener("click", () => {
+  navBar.classList.toggle("toggle");
+});
+navLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("clicked a menu link !");
+    navBar.classList.remove("toggle");
   });
-
-const toggleButton = document.querySelector('.toggle-menu');
-const navBar = document.querySelector('.nav-bar');
-const navLink  = document.querySelectorAll('.nav-link');
-toggleButton.addEventListener('click',()=>{
-    navBar.classList.toggle('toggle');
-});
-navLink.forEach(item=>{
-    item.addEventListener('click',()=>{
-        console.log('clicked a menu link !');
-        navBar.classList.remove('toggle');
-    });
 });
 
-function sendMail() {
-    var link = "mailto:omkar.webdev@gmail.com"
-             + "&subject=" + encodeURIComponent("This is my subject")
-             + "&body=" + encodeURIComponent(document.getElementById('textarea').value)
-    ;
-    
-    window.location.href = link;
-}
+const sendMail = () => {
+  var link =
+    "mailto:omkar.webdev@gmail.com" +
+    "&subject=" +
+    encodeURIComponent("This is my subject") +
+    "&body=" +
+    encodeURIComponent(document.getElementById("textarea").value);
+  window.location.href = link;
+};
+
+const switchTab = (tab) => {
+  var i;
+  var x = document.getElementsByClassName("box");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tab).style.display = "block";
+};
